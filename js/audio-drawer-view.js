5b39e5bc854347c9ba3659b84f5d7b5e763f02b4
RobertPeek <robert@delta-net.co.uk>
2017-09-07 15:27:32 +0100

    Fixes #117

--- js/audio-drawer-view.js
+++ js/audio-drawer-view.js
@@ -74,11 +74,11 @@ define(function(require) {
 
         checkTextSize: function() {
             if(Adapt.audio.textSize==0){
-                this.$('.text-description').html(Adapt.course.get('_audio')._reducedText.descriptionFull);
+                this.$('.text-description').html(Adapt.course.get('_audio')._reducedText.descriptionFull).a11y_text();
                 this.$('.full-button').hide();
                 this.$('.reduced-button').show();
             } else {
-                this.$('.text-description').html(Adapt.course.get('_audio')._reducedText.descriptionReduced);
+                this.$('.text-description').html(Adapt.course.get('_audio')._reducedText.descriptionReduced).a11y_text();
                 this.$('.reduced-button').hide();
                 this.$('.full-button').show();
             }
@@ -86,11 +86,11 @@ define(function(require) {
 
         checkNarration: function() {
             if(Adapt.audio.audioClip[0].status==1){
-                this.$('.narration-description').html(Adapt.course.get('_audio')._channels._narration.descriptionOn);
+                this.$('.narration-description').html(Adapt.course.get('_audio')._channels._narration.descriptionOn).a11y_text();
                 this.$('.item-narration').removeClass(Adapt.audio.iconOff);
                 this.$('.item-narration').addClass(Adapt.audio.iconOn);
             } else {
-                this.$('.narration-description').html(Adapt.course.get('_audio')._channels._narration.descriptionOff);
+                this.$('.narration-description').html(Adapt.course.get('_audio')._channels._narration.descriptionOff).a11y_text();
                 this.$('.item-narration').removeClass(Adapt.audio.iconOn);
                 this.$('.item-narration').addClass(Adapt.audio.iconOff);
             }
@@ -98,11 +98,11 @@ define(function(require) {
 
         checkEffects: function() {
             if(Adapt.audio.audioClip[1].status==1){
-                this.$('.effects-description').html(Adapt.course.get('_audio')._channels._effects.descriptionOn);
+                this.$('.effects-description').html(Adapt.course.get('_audio')._channels._effects.descriptionOn).a11y_text();
                 this.$('.item-effects').removeClass(Adapt.audio.iconOff);
                 this.$('.item-effects').addClass(Adapt.audio.iconOn);
             } else {
-                this.$('.effects-description').html(Adapt.course.get('_audio')._channels._effects.descriptionOff);
+                this.$('.effects-description').html(Adapt.course.get('_audio')._channels._effects.descriptionOff).a11y_text();
                 this.$('.item-effects').removeClass(Adapt.audio.iconOn);
                 this.$('.item-effects').addClass(Adapt.audio.iconOff);
             }
@@ -110,11 +110,11 @@ define(function(require) {
 
         checkMusic: function() {
             if(Adapt.audio.audioClip[2].status==1){
-                this.$('.music-description').html(Adapt.course.get('_audio')._channels._music.descriptionOn);
+                this.$('.music-description').html(Adapt.course.get('_audio')._channels._music.descriptionOn).a11y_text();
                 this.$('.item-music').removeClass(Adapt.audio.iconOff);
                 this.$('.item-music').addClass(Adapt.audio.iconOn);
             } else {
-                this.$('.music-description').html(Adapt.course.get('_audio')._channels._music.descriptionOff);
+                this.$('.music-description').html(Adapt.course.get('_audio')._channels._music.descriptionOff).a11y_text();
                 this.$('.item-music').removeClass(Adapt.audio.iconOn);
                 this.$('.item-music').addClass(Adapt.audio.iconOff);
             }
