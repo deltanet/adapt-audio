e3ccb321d899f2bc1dd1d3a1aecda9111e26a549
Robert Peek <RobertPeek@users.noreply.github.com>
2019-06-28 16:56:16 +0100

    Removed reference to Accessibility toggle

--- js/audio-navigation-view.js
+++ js/audio-navigation-view.js
@@ -8,8 +8,7 @@ define([
 
         initialize: function() {
             this.listenTo(Adapt, {
-                "audio:updateAudioStatus": this.updateToggle,
-                "accessibility:toggle": this.onAccessibilityToggle
+                "audio:updateAudioStatus": this.updateToggle
             });
 
             this.render();
@@ -56,35 +55,6 @@ define([
             if (event) event.preventDefault();
 
             Adapt.trigger('audio:showAudioDrawer');
-        },
-
-        onAccessibilityToggle: function() {
-          if (Adapt.config.has('_accessibility') && Adapt.config.get('_accessibility')._isEnabled) {
-
-            if (Adapt.config.get('_accessibility')._isActive) {
-
-              this.$el.addClass('hidden');
-
-              for (var i = 0; i < Adapt.audio.numChannels; i++) {
-                Adapt.trigger('audio:updateAudioStatus', i, 0);
-              }
-
-              Adapt.trigger('popup:closed');
-              Adapt.trigger('notify:closed');
-
-            } else {
-              // Set defaults
-              if (Adapt.course.get('_audio')._showOnNavbar) {
-                this.$el.removeClass('hidden');
-              }
-
-              Adapt.trigger('audio:updateAudioStatus', 0, Adapt.course.get('_audio')._channels._narration._status);
-              Adapt.trigger('audio:updateAudioStatus', 1, Adapt.course.get('_audio')._channels._effects._status);
-              Adapt.trigger('audio:updateAudioStatus', 2, Adapt.course.get('_audio')._channels._music._status);
-
-              Adapt.audio.audioStatus = Adapt.course.get('_audio')._channels._narration._status;
-            }
-          }
         }
 
     });
