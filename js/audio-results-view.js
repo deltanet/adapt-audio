93bd4081437f53ce7bc6eec70a937289b1822654
Robert Peek <>
2020-11-26 09:40:35 +0000

    Update for results audio button

--- js/audio-results-view.js
+++ js/audio-results-view.js
@@ -9,7 +9,7 @@ define([
     initialize: function () {
       this.listenTo(Adapt, {
         'remove': this.remove,
-        'audio:updateAudioStatus': this.updateToggle
+        'audio:updateAudioStatus device:resize': this.updateToggle
       });
 
       this.listenToOnce(Adapt, 'remove', this.removeInViewListeners);
@@ -37,6 +37,8 @@ define([
       // Add audio icon
       this.$('.audio__controls-icon').addClass(this.audioIcon);
 
+      this.elementHeight = this.$('.audio__controls').outerHeight();
+
       // Hide controls
       if (this.model.get('_audioAssessment')._showControls == false || Adapt.audio.audioClip[this.audioChannel].status == 0) {
         this.$('.audio__controls').addClass('hidden');
@@ -47,6 +49,14 @@ define([
 
       // Set listener for when clip ends
       $(Adapt.audio.audioClip[this.audioChannel]).on('ended', this.onAudioEnded.bind(this));
+
+      _.defer(function () {
+        this.postRender();
+      }.bind(this));
+    },
+
+    postRender: function () {
+      this.updateToggle();
     },
 
     onAudioEnded: function() {
@@ -54,8 +64,37 @@ define([
     },
 
     updateToggle: function() {
+      // Reset
+      $('.'+this.elementId).find('.component__body-inner').css('max-width', "");
+      $('.'+this.elementId).find('.component__title-inner').css('max-width', "");
+
+      this.$('.audio__controls').css('padding', "");
+      this.$('.audio__controls').css('height', "");
+      this.$('.audio__controls').css('width', "");
+
       if (Adapt.audio.audioClip[this.audioChannel].status == 1 && this.model.get('_audioAssessment')._showControls) {
         this.$('.audio__controls').removeClass('hidden');
+
+        var outerWidth = this.$('.js-audio-toggle').outerWidth();
+        var elementWidth = $('.'+this.elementId).find('.component-header').outerWidth();
+        var padding = outerWidth - this.$('.js-audio-toggle').width();
+        var maxWidth = (elementWidth - outerWidth) - padding;
+
+        var titleHeight = $('.'+this.elementId).find('.component__title').outerHeight();
+
+        // Set width on elements title or body
+        if (this.model.get('displayTitle') == "") {
+          $('.'+this.elementId).find('.component__body-inner').css('max-width', maxWidth);
+        } else {
+          $('.'+this.elementId).find('.component__title-inner').css('max-width', maxWidth);
+
+          if (titleHeight < this.elementHeight) {
+            this.$('.audio__controls').css('padding', 0);
+            this.$('.audio__controls').css('height', titleHeight);
+            this.$('.audio__controls').css('width', titleHeight);
+          }
+        }
+
       } else {
         this.$('.audio__controls').addClass('hidden');
       }
