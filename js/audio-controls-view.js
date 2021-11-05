dfe3bc4e7dab6cb9011e4cf6c28dc003bf809408
Robert Peek <RobertPeek@users.noreply.github.com>
2021-11-05 12:52:33 +0000

    Fixes #201

--- js/audio-controls-view.js
+++ js/audio-controls-view.js
@@ -286,7 +286,7 @@ define([
           this.setAudioFile();
 
           // Check for component items
-          if (this.elementType === 'component' && !this.model.get('_isQuestionType') && this.model.get('_children')) {
+          if (this.elementType === 'component' && this.model.get('_items') && !this.model.get('_isQuestionType') && this.model.get('_children')) {
             var itemIndex = this.getActiveItemIndex();
             var currentItem = this.model.get('_items')[itemIndex];
 
@@ -363,7 +363,7 @@ define([
       Adapt.audio.audioClip[this.audioChannel].src = this.audioFile;
 
       // Check for component items
-      if (this.elementType === 'component' && !this.model.get('_isQuestionType') && this.model.get('_children')) {
+      if (this.elementType === 'component' && this.model.get('_items') && !this.model.get('_isQuestionType') && this.model.get('_children')) {
         var itemIndex = this.getActiveItemIndex();
         var currentItem = this.model.get('_items')[itemIndex];
 
